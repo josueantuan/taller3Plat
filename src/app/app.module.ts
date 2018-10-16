a16e95e99f0a486a591ff58c9fb2ebf4ce2d8955
josueantuan <antuan_1996@hotmail.com>
2018-10-15 22:07:37 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MapaMComponent } from './components/mapa-m/mapa-m.component';
+import { MapaSComponent } from './components/mapa-s/mapa-s.component';
+import { MapaDComponent } from './components/mapa-d/mapa-d.component';
+import { AgmCoreModule } from '@agm/core';
+import { AgmDirectionModule } from 'agm-direction'
+//-----------------------------Rutas----------------------------------
+import { appRoutingProviders, routing } from "./app.routing";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MapaMComponent,
+    MapaSComponent,
+    MapaDComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBbsOlMryAHu2ESwHHSwrDBIUU7fiENNoM'
+    }),
+    AgmDirectionModule
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
